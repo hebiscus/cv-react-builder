b0ce7b970feecd31a7d09e496348b74430a71a40
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-05-16 18:20:50 +0200

    changing from function to class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
 import './styles/App.css';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/GeneralInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+class GeneralInfo extends Component {
+    render() {
+        return <div>
+
+        </div>
+    }
+}
+
+export default GeneralInfo;
