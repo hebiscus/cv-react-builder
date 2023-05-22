3dfc05bcdc9461184c96ce8d47243e7c8f5722c7
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-05-22 21:38:57 +0200

    App component changed to a functional one

--- src/App.js
+++ src/App.js
@@ -5,27 +5,41 @@ import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
 import Experience from './components/Experience';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
 
-    }
-  }
+//     }
+//   }
  
 
-  render () {
-    return (
-      <div className='app-content'>
-        <h3>Personal Details</h3>
-        <GeneralInfo />
-        <h3>Professional experience</h3>
-        <Experience />
-        <h3>Education</h3>
-        <Education />
-      </div>
-    );
-  }
+//   render () {
+//     return (
+//       <div className='app-content'>
+//         <h3>Personal Details</h3>
+//         <GeneralInfo />
+//         <h3>Professional experience</h3>
+//         <Experience />
+//         <h3>Education</h3>
+//         <Education />
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+
+  return (
+    <div className='app-content'>
+      <h3>Personal Details</h3>
+      <GeneralInfo />
+      <h3>Professional experience</h3>
+      <Experience />
+      <h3>Education</h3>
+      <Education />
+    </div>
+  )
 }
 
 export default App;
