a0a2c55f3f95e0ed9bd042952f0d659fa56b0137
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-05-23 21:21:56 +0200

    education refactor to functional component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,78 +1,66 @@
-import React from 'react';
-import { Component } from 'react';
+import { useState } from 'react';
 import '../styles/App.css';
 
-class Education extends Component {
-    constructor() {
-        super();
-        this.state = {
+const Education = () =>  {
+    const initialValues = {
             program: "",
             university: "",
             startDate: "",
             endDate: "",
-            submitStatus: "edit",
-            savedInputs: [],
         };
-        this.onInputChange = this.onInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleEdit = this.handleEdit.bind(this);
-    }
 
-    onInputChange(event) {
-        const name = event.target.name;
-        this.setState({
-          [name]: event.target.value
+    const [inputValues, setInputValues] = useState(initialValues)
+    const [submitStatus, setSubmitStatus] = useState("edit");
+    const [savedInputs, setInputs] = useState([]);
+    const editStatus = submitStatus === "edit"
+    
+    const updateInputs = (event) => {
+        const { name, value } = event.target;
+
+        setInputValues({
+            ...inputValues,
+            [name]: value,
         });
-    }
+    };
 
-    handleSubmit(e) {
-        if (this.state.program === "" || this.state.university === "") {
+    const handleSubmit = (e) => {
+        if (inputValues.program === "" || inputValues.university === "") {
             return;
         }
         e.preventDefault();
-        const inputsArray = [this.state.program, this.state.university, this.state.startDate, this.state.endDate];
-        if (this.state.savedInputs.length === 0) {
-            this.setState({
-                submitStatus: "submitted",
-                savedInputs: this.state.savedInputs.concat(inputsArray)
-            });
-            return
-        } 
-        this.setState({
-            submitStatus: "submitted",
-            savedInputs: this.state.savedInputs.map((input, index) => inputsArray[index])
-        });
-    }
+        setInputs([
+            ...Object.values(inputValues)
+        ]);
+        setSubmitStatus("submit")
+    };
 
-    handleEdit() {
-        this.setState({submitStatus: "edit"});
-    }
+    const handleEdit = () => {
+        setSubmitStatus("edit")
+    };
 
-    render() {
-        const editStatus = this.state.submitStatus === "edit";
-        const sectionInputs = this.state.savedInputs;
-        return <div>
-            {editStatus 
-            ? <div className='education-edit'>
-                <form className='general-form'>
-                    <input name='program' type='text' placeholder="program/course" defaultValue={this.state.program} onChange={this.onInputChange} required></input>
-                    <input name='university' type='text' placeholder="university" defaultValue={this.state.university} onChange={this.onInputChange} required></input>
-                    <input name='startDate' type='date' placeholder="start date" defaultValue={this.state.startDate} onChange={this.onInputChange} required></input>
-                    <input name='endDate' type='date' placeholder="end date" defaultValue={this.state.endDate} onChange={this.onInputChange} required></input>
-                    <button type='submit' onClick={this.handleSubmit}>Submit Section</button>
-                </form>
-            </div>
-            : <div className='education-submitted'>
-                {sectionInputs.map(input => {
-                    return <div>
-                        {input}
-                    </div>
-                })}
-                <button onClick={this.handleEdit}>Edit</button>
-            </div> 
-            }
+    return ( 
+        <div>
+        {editStatus 
+            ?   <div className='education-edit'>
+                    <form className='general-form'>
+                        <input name='program' type='text' placeholder="program/course" value={inputValues.program} onChange={updateInputs} required></input>
+                        <input name='university' type='text' placeholder="university" value={inputValues.university} onChange={updateInputs} required></input>
+                        <input name='startDate' type='date' placeholder="start date" value={inputValues.startDate} onChange={updateInputs} required></input>
+                        <input name='endDate' type='date' placeholder="end date" value={inputValues.endDate} onChange={updateInputs} required></input>
+                        <button type='submit' onClick={handleSubmit}>Submit Section</button>
+                    </form>
+                </div>
+            :   <div className='education-submitted'>
+                    {savedInputs.map(input => {
+                        return <div>
+                            {input}
+                        </div>
+                    })}
+                    <button onClick={handleEdit}>Edit</button>
+                </div> 
+        }
         </div>
-    }
-}
+    );
+};
 
 export default Education;
